da83ada135584cafc4e3c90e65b90daa6db0fa3c
Willi Schönborn <w.schoenborn@gmail.com>
2011-12-22 01:19:38 +0100

    added automatic hadoop download

--- bin/install.sh
+++ bin/install.sh
@@ -1,15 +1,24 @@
 #!/bin/sh
 
+if [ ! -d hadoop ]; then
+    rm -rvf hadoop
+
+    wget http://apache.prosite.de//hadoop/common/hadoop-0.20.203.0/hadoop-0.20.203.0rc1.tar.gz || exit $?
+    tar -xvzf hadoop-0.20.203.0rc1.tar.gz || exit $?
+    mv hadoop-0.20.203.0 hadoop || exit $?
+    rm -vf hadoop-0.20.203.0rc1.tar.gz || exit $?
+fi
+
 mvn install:install-file \
 -Dfile=deps/com.inamik.utils.tableformatter-0.96.2.jar \
 -DgroupId=com.inamik.utils \
 -DartifactId=tableformatter \
 -Dversion=0.96.2 \
--Dpackaging=jar
+-Dpackaging=jar || exit $?
 
 mvn install:install-file \
 -Dfile=deps/jsqlparser-0.7.0.jar \
 -DgroupId=jsqlparser \
 -DartifactId=jsqlparser \
 -Dversion=0.7.0 \
--Dpackaging=jar
+-Dpackaging=jar || exit $?
